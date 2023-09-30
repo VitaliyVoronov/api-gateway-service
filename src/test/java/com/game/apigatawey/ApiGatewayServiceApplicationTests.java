09610e75e6d178f5571d37bca3c2c24801084b03
Vitaliy <vitalik.voronov@gmail.com>
2023-09-30 12:20:51 +0200

    first run

---
+++ src/test/java/com/game/apigatawey/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.apigatawey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
